7689db9adf28409c28129fcbf6b43071dfd446f9
ᎹᎦᎵ <m.conrad.202@gmail.com>
2022-01-23 16:26:52 -0500

    tqdm + wax cylindar noise

--- create_vocoder_files.py
+++ create_vocoder_files.py
@@ -13,9 +13,9 @@ import random
 import shutil
 import os
 import sys
-from progressbar import ProgressBar
 from pydub import AudioSegment
 from typing import List
+from tqdm import tqdm
 
 
 def main():
@@ -44,27 +44,21 @@ def main():
         all_audio_files.extend(glob.glob(f"{audio_file_glob}"))
 
     print("Creating clean audio files")
-    bar: ProgressBar = ProgressBar(maxval=len(all_audio_files))
-    bar.start()
     idx: int = 0
-    for audio_file in all_audio_files:
+    for audio_file in tqdm(all_audio_files):
         idx += 1
         audio_file_noisy: str = f"{idx:09d}_clean.wav"
         audio: AudioSegment = AudioSegment.from_file(audio_file)
         audio = audio.set_channels(1).set_frame_rate(output_sr)
         audio.export(os.path.join(dest_audio_dir, audio_file_noisy), format="wav")
-        bar.update(bar.currval + 1)
-    bar.finish()
 
     print("Creating noisy audio files")
     noise_audio_segments: List[AudioSegment] = list()
     for noise_file in glob.glob(noise_audio_glob):
         noise_audio_segments.append(AudioSegment.from_file(noise_file).set_frame_rate(output_sr).set_channels(1))
     ran: random.Random = random.Random(len(noise_audio_segments))
-    bar: ProgressBar = ProgressBar(maxval=len(all_audio_files))
-    bar.start()
     idx: int = 0
-    for audio_file in all_audio_files:
+    for audio_file in tqdm(all_audio_files):
         idx += 1
         ran.shuffle(noise_audio_segments)
         noise_overlay: AudioSegment = AudioSegment.silent(frame_rate=output_sr)
@@ -79,8 +73,6 @@ def main():
             audio = audio.set_frame_rate(4000).set_frame_rate(output_sr)
         audio = audio.set_channels(1)
         audio.export(os.path.join(dest_audio_dir, audio_file_noisy), format="wav")
-        bar.update(bar.currval + 1)
-    bar.finish()
 
 
 if __name__ == "__main__":
