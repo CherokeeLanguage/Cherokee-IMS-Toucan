e277a8faa518b0451b0644f4c4d52c9dc76d21f8
ᎹᎦᎵ <m.conrad.202@gmail.com>
2022-01-23 13:26:04 -0500

    sync

--- HiFiGAN_combined.py
+++ HiFiGAN_combined.py
@@ -1,44 +1,44 @@
+#!/usr/bin/env bash
+"""true" '''\'
+set -e
+eval "$(conda shell.bash hook)"
+conda deactivate
+conda activate toucan_conda_venv
+exec python "$0" "$@"
+exit $?
+''"""
+import os
 import random
+import shutil
 
 import torch
 
 from IMS_Toucan.TrainingInterfaces.Spectrogram_to_Wave.HiFIGAN.HiFiGAN import HiFiGANGenerator
 from IMS_Toucan.TrainingInterfaces.Spectrogram_to_Wave.HiFIGAN.HiFiGAN import HiFiGANMultiScaleMultiPeriodDiscriminator
-from IMS_Toucan.TrainingInterfaces.Spectrogram_to_Wave.HiFIGAN.HiFiGANDataset import HiFiGANDataset
+from IMS_Toucan.TrainingInterfaces.Spectrogram_to_Wave.HiFIGAN.HiFiGANDataset_disklist import HiFiGANDataset
 from IMS_Toucan.TrainingInterfaces.Spectrogram_to_Wave.HiFIGAN.hifigan_train_loop import train_loop
 from Utility.file_lists import *
 
 
-def run(gpu_id, resume_checkpoint, finetune, model_dir):
-    if gpu_id == "cpu":
-        os.environ["CUDA_VISIBLE_DEVICES"] = ""
-        device = torch.device("cpu")
-
-    else:
-        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-        os.environ["CUDA_VISIBLE_DEVICES"] = "{}".format(gpu_id)
-        device = torch.device("cuda")
+def main():
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "{}".format(0)
+    device = torch.device("cuda")
 
     torch.manual_seed(131714)
     random.seed(131714)
     torch.random.manual_seed(131714)
 
     print("Preparing")
-    if model_dir is not None:
-        model_save_dir = model_dir
-    else:
-        model_save_dir = "Models/HiFiGAN_combined"
+    model_save_dir = os.path.join("IMS_Toucan", "Models", "HiFiGAN_combined")
     if not os.path.exists(model_save_dir):
         os.makedirs(model_save_dir)
 
-    file_lists = list()
-    file_lists.append(get_file_list_cstr_vctk_corpus())
+    shutil.rmtree("_cache", ignore_errors=True)
+    os.makedirs("_cache", exist_ok=True)
 
-    datasets = list()
-
-    for file_list in file_lists:
-        datasets.append(HiFiGANDataset(list_of_paths=file_list))
-    train_set = torch.utils.data.ConcatDataset(datasets)
+    file_list: List[str] = get_hifigan_wav_list()
+    train_set = HiFiGANDataset(list_of_paths=file_list, cache_dir="_cache", loading_processes=1)
 
     generator = HiFiGANGenerator()
     generator.reset_parameters()
@@ -46,11 +46,15 @@ def run(gpu_id, resume_checkpoint, finetune, model_dir):
 
     print("Training model")
     train_loop(batch_size=16,
-               steps=2000000,
+               steps=2_000_000,
                generator=generator,
                discriminator=multi_scale_discriminator,
                train_dataset=train_set,
                device=device,
                epochs_per_save=1,
                model_save_dir=model_save_dir,
-               path_to_checkpoint=resume_checkpoint)
+               path_to_checkpoint=os.path.join(model_save_dir, "checkpoint_328158.pt"))
+
+
+if __name__ == "__main__":
+    main()
--- Utility/file_lists.py
+++ Utility/file_lists.py
@@ -1,20 +1,12 @@
+import glob
 import os
 
 from typing import List
 
-from typing import List
 
-
-def get_file_list_cstr_vctk_corpus() -> List[str]:
-    root = os.path.abspath("data/other-audio-data/cstr-vctk-corpus")
+def get_hifigan_wav_list() -> List[str]:
     file_list: List[str] = list()
-    with open(os.path.join(root, "cstr-vctk-corpus-48k.txt")) as file:
-        for line in file:
-            line = line.strip()
-            if not line:
-                continue
-            wav_file = os.path.splitext(line.split("|")[3])[0]+".wav"
-            wav_path = os.path.join(root, "wavs", wav_file)
-            if os.path.exists(wav_path):
-                file_list.append(wav_path)
+    root = os.path.abspath(os.path.join("IMS_Toucan", "audios", "vocoder"))
+    for wav in glob.glob(os.path.join(root, "*_clean.wav")):
+        file_list.append(wav)
     return file_list
